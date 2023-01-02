2dcdd0d167b77543a9864760cbee0e5a18a87fd5
Nicolas Lardot <nicolas.lardot@neopixl.com>
2023-01-02 23:59:23 +0100

    feat: add custom authenticator

--- src/Controller/GoogleController.php
+++ src/Controller/GoogleController.php
@@ -11,33 +11,33 @@ use Symfony\Component\HttpFoundation\Request;
 
 class GoogleController extends AbstractController
 {
-    #[Route('/connect/check', name: 'connect_google_check')]
-    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry): JsonResponse
+    #[Route('/connect/google/check', name: 'connect_google_check')]
+    public function connectCheckAction(Request $request, ClientRegistry $clientRegistry)
     {
-        // ** if you want to *authenticate* the user, then
-        // leave this method blank and create a Guard authenticator
-        // (read below)
+        // // ** if you want to *authenticate* the user, then
+        // // leave this method blank and create a Guard authenticator
+        // // (read below)
 
-        /** @var \KnpU\OAuth2ClientBundle\Client\Provider\GoogleClient $client */
-        $client = $clientRegistry->getClient('google');
+        // /** @var \KnpU\OAuth2ClientBundle\Client\Provider\GoogleClient $client */
+        // $client = $clientRegistry->getClient('google');
 
-        try {
-            // the exact class depends on which provider you're using
-            /** @var \League\OAuth2\Client\Provider\GoogleUser $user */
-            $user = $client->fetchUser();
+        // try {
+        //     // the exact class depends on which provider you're using
+        //     /** @var \League\OAuth2\Client\Provider\GoogleUser $user */
+        //     $user = $client->fetchUser();
 
-            // do something with all this new power!
-            // e.g. $name = $user->getFirstName();
-            // var_dump($user); die;
-            // ...
+        //     // do something with all this new power!
+        //     // e.g. $name = $user->getFirstName();
+        //     // var_dump($user); die;
+        //     // ...
 
-            dd($user);
+        //     dd($user);
 
-        } catch (IdentityProviderException $e) {
-            // something went wrong!
-            // probably you should return the reason to the user
-            var_dump($e->getMessage()); die;
-        }
+        // } catch (IdentityProviderException $e) {
+        //     // something went wrong!
+        //     // probably you should return the reason to the user
+        //     var_dump($e->getMessage()); die;
+        // }
     }
 
     #[Route('/connect/google', name: 'app_google_connect')]
@@ -45,7 +45,7 @@ class GoogleController extends AbstractController
     {
         return $clientRegistry
             ->getClient('google')
-            ->redirect(['email']);
+            ->redirect([], ['prompt' => 'consent']);
     }
 
 }
